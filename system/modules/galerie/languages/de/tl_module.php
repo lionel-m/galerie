be7a72d1158e55137192e153d70f1d701c2a92de
lionel-m <lionel.maccaud@gmail.com>
2012-12-07 14:01:03 +0100

    Add new language file for DE

---
+++ system/modules/galerie/languages/de/tl_module.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * TL_ROOT/system/modules/galerie/languages/de/tl_module.php 
+ * 
+ * Contao extension: galerie 1.4.0 stable 
+ * German translation file 
+ * 
+ * Copyright : Lionel Maccaud 
+ * License   : MIT 
+ * Author    : Lionel Maccaud (lionel), http://lionelmaccaud.ch
+ * Translator: Carolina Koehn (lucina), http://kikmedia.de
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['galerie']['0'] = "Galleria";
+$GLOBALS['TL_LANG']['tl_module']['galerie']['1'] = "Galerie auswählen";
+$GLOBALS['TL_LANG']['tl_module']['galerie_legend'] = "Wählen Sie Ihre Galerie aus.";
+?>
